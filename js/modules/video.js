d721417d2bb0ceb431ef96418d24443442bca5c5
Fearnall <l_fearnall@tss-hr420-dm08.fc.ca>
2017-12-06 16:34:58 -0500

    video mouseover effect

--- js/modules/video.js
+++ js/modules/video.js
@@ -16,10 +16,27 @@ var video = {
     video.volumeIndicator.classList.replace('fa-volume-up', 'fa-volume-off');
   },
 
+  popOverlay() {
+    let overlay = document.querySelector('.vid-overlay');
+    overlay.classList.add('show-overlay');
+
+    overlay.querySelector('i').addEventListener('click', video.replayVideo);
+  },
+
+  replayVideo() {
+    //rewind and replay the video from the beginning
+    video.videoPlayer.currentTime = 0;
+    video.videoPlayer.play();
+
+    let overlay = document.querySelector('.vid-overlay');
+    overlay.classList.remove('show-overlay');
+  },
+
   init() {
     console.log('added a video module');
     video.videoPlayer.addEventListener('mouseover', video.volOn);
     video.videoPlayer.addEventListener('mouseout', video.volOff);
+    video.videoPlayer.addEventListener('ended', video.popOverlay);
   }
 }
 
